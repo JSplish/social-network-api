1ac37a4074e40e7f261182b81b520b9f4bff900a
Jordan <jordan.m.edginton@gmail.com>
2022-05-23 17:01:44 -0600

    user model created

---
+++ models/User.js
@@ -0,0 +1,49 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema({
+
+      username: {
+        type: String,
+        required: 'username is Required',
+        unique: true,
+        trim: true
+      },
+      // password: {
+      //   type: String,
+      //   trim: true,
+      //   required: 'Password is Required',
+      //   minlength: 6
+      // },
+      email: {
+        type: String,
+        unique: true,
+        match: [/.+@.+\..+/, 'Please enter a valid e-mail address'],
+        required: true
+      },
+      thoughts: [
+        {
+          type: Schema.Types.ObjectId,
+          ref: "Thought"
+        }
+      ],
+      friends: [
+        {
+          type: Schema.Types.ObjectId,
+          ref: "User"
+        }
+      ],
+      toJson: {
+        virtuals: true,
+        getters: true
+      }
+});
+
+UserSchema.virtual("friendCount").get(function() {
+  return this.friends(
+
+  )
+});
+
+const User = model('User', UserSchema);
+
+module.exports = User;
